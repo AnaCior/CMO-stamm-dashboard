fba848887abf986f0bc9dbf226e6777e8df0fe98
AnaCior <118361884+AnaCior@users.noreply.github.com>
2024-12-16 18:41:31 +0100

    Add files via upload

---
+++ engdict.py
@@ -0,0 +1,230 @@
+# Define the themes and their corresponding markdown content
+
+Themes = {
+    "Satisfaction with life": """
+        **Welcome to the Life Satisfaction indicator**
+        - This indicator is part of the Subjective Well-being theme.
+        - Preliminary figures. When a new year is added, the model re-estimates 
+        all years in the series. Refer to the Technical Explanation for more 
+        details on interpreting model estimates and margins.
+    """,
+    "Leisure Satisfaction": """
+        **Welcome to the Leisure Satisfaction indicator**
+        - This indicator is part of the Subjective Well-being theme.
+        - Preliminary figures. When a new year is added, the model re-estimates 
+        all years in the series. Refer to the Technical Explanation for more 
+        details on interpreting model estimates and margins.
+    """,
+    "Median Disposable Income": """
+        **Welcome to the Median Disposable Income indicator**
+        - This indicator is part of the Material Well-being theme.
+        - 2021 figures are preliminary, and the adjustment for price changes in 
+        2021 is based on the consumer price research series, which uses actual 
+        energy prices paid. This aligns more closely with the price developments 
+        experienced by the population than the consumer price index.
+    """,
+    "Gross Domestic Product": """
+        **Welcome to the Gross Domestic Product indicator**
+        - This indicator is part of the Material Well-being theme.
+        - 2022 figures are preliminary.
+    """,
+    "Overweight": """
+        **Welcome to the Overweight indicator**
+        - This indicator is part of the Health theme.
+        - For the year 2016, the measured population is 19+.
+    """,
+    "Perceived Health": """
+        **Welcome to the Perceived Health indicator**
+        - This indicator is part of the Health theme.
+        - For the years 2012 and 2016, the measured population is 19+.
+    """,
+    "Life Expectancy of the Population": """
+        **Welcome to the Life Expectancy of the Population indicator**
+        - This indicator is part of the Health theme.
+        - Average life expectancy over the period 2018-2021, not determined 
+        separately per year.
+    """,
+    "People with One or More Chronic Illnesses or Conditions": """
+        **Welcome to the People with One or More Chronic Illnesses or Conditions indicator**
+        - This indicator is part of the Health theme.
+        - For the years 2012 and 2016, the measured population is 19+.
+    """,
+    "Net Labor Participation": """
+        **Welcome to the Net Labor Participation indicator**
+        - This indicator is part of the Work and Leisure theme.
+    """,
+    "Gross Labor Participation": """
+        **Welcome to the Gross Labor Participation indicator**
+        - This indicator is part of the Work and Leisure theme.
+    """,
+    "Highly Educated Population": """
+        **Welcome to the Highly Educated Population indicator**
+        - This indicator is part of the Work and Leisure theme.
+    """,
+    "Unemployment": """
+        **Welcome to the Unemployment indicator**
+        - This indicator is part of the Work and Leisure theme.
+    """,
+    "Vacancy Rate": """
+        **Welcome to the Vacancy Rate indicator**
+        - This indicator is part of the Work and Leisure theme.
+        - 2021 and 2022 figures are preliminary.
+    """,
+    "Distance to Public Transport": """
+        **Welcome to the Distance to Public Transport indicator**
+        - This indicator is part of the Work and Leisure theme.
+    """,
+    "Satisfaction with Living Environment": """
+        **Welcome to the Satisfaction with Living Environment indicator**
+        - This indicator is part of the Housing theme.
+    """,
+    "Satisfaction with Housing": """
+        **Welcome to the Satisfaction with Housing indicator**
+        - This indicator is part of the Housing theme.
+    """,
+    "Distance to Sports Facility": """
+        **Welcome to the Distance to Sports Facility indicator**
+        - This indicator is part of the Housing theme.
+    """,
+    "Distance to Primary School": """
+        **Welcome to the Distance to Primary School indicator**
+        - This indicator is part of the Housing theme.
+    """,
+    "Distance to Café, etc.": """
+        **Welcome to the Distance to Café, etc. indicator**
+        - This indicator is part of the Housing theme.
+    """,
+    "Contact with Family, Friends, or Neighbors": """
+        **Welcome to the Contact with Family, Friends, or Neighbors indicator**
+        - This indicator is part of the Society theme.
+        - Preliminary figures. When a new year is added, the model re-estimates 
+        all years in the series. Refer to the Technical Explanation for more 
+        details on interpreting model estimates and margins.
+    """,
+    "Trust in Institutions": """
+        **Welcome to the Trust in Institutions indicator**
+        - This indicator is part of the Society theme.
+        - Preliminary figures. When a new year is added, the model re-estimates 
+        all years in the series. Refer to the Technical Explanation for more 
+        details on interpreting model estimates and margins.
+    """,
+    "Trust in Others": """
+        **Welcome to the Trust in Others indicator**
+        - This indicator is part of the Society theme.
+        - Preliminary figures. When a new year is added, the model re-estimates 
+        all years in the series. Refer to the Technical Explanation for more 
+        details on interpreting model estimates and margins.
+    """,
+        "Volunteering": """
+        **Welcome to the Volunteering indicator**
+        - This indicator is part of the Society theme.
+        - Preliminary figures. When a new year is added, the model re-estimates 
+        all years in the series. Refer to the Technical Explanation for more 
+        details on interpreting model estimates and margins.
+    """,
+    "Often Feeling Unsafe in the Neighborhood": """
+        **Welcome to the Often Feeling Unsafe in the Neighborhood indicator**
+        - This indicator is part of the Safety theme.
+    """,
+    "Number of Experienced Crimes": """
+        **Welcome to the Number of Experienced Crimes indicator**
+        - This indicator is part of the Safety theme.
+    """,
+    "Registered Crimes": """
+        **Welcome to the Registered Crimes indicator**
+        - This indicator is part of the Safety theme.
+        - 2021 and 2022 figures are preliminary.
+    """,
+    "Nature Area per Inhabitant": """
+        **Welcome to the Nature Area per Inhabitant indicator**
+        - This indicator is part of the Environment theme.
+    """,
+    "Fine Particulate Matter Emissions into Air": """
+        **Welcome to the Fine Particulate Matter Emissions into Air indicator**
+        - This indicator is part of the Environment theme.
+    """,
+    "Distance to Public Green Spaces": """
+        **Welcome to the Distance to Public Green Spaces indicator**
+        - This indicator is part of the Environment theme.
+    """,
+    "Nature and Forest Areas": """
+        **Welcome to the Nature and Forest Areas indicator**
+        - This indicator is part of the Environment theme.
+    """,
+    "Greenhouse Gas Emissions per Inhabitant": """
+        **Welcome to the Greenhouse Gas Emissions per Inhabitant indicator**
+        - This indicator is part of the Environment theme.
+        - The entire series has been adjusted due to new IPCC guidelines.
+    """,
+    "Quality of Swimming Water in Inland Waters": """
+        **Welcome to the Quality of Swimming Water in Inland Waters indicator**
+        - This indicator is part of the Environment theme.
+    """,
+    "Quality of Swimming Water in Coastal Waters": """
+        **Welcome to the Quality of Swimming Water in Coastal Waters indicator**
+        - This indicator is part of the Environment theme.
+    """,
+    "Average Household Debt": """
+        **Welcome to the Average Household Debt indicator**
+        - This indicator is part of the Economic Capital theme.
+        - 2022 figures are preliminary, and the adjustment for price changes 
+        in 2021 and 2022 is based on the consumer price research series, which 
+        uses actual energy prices paid. This aligns more closely with the price 
+        developments experienced by the population than the consumer price index.
+    """,
+    "Median Household Wealth": """
+        **Welcome to the Median Household Wealth indicator**
+        - This indicator is part of the Economic Capital theme.
+        - 2021 figures are preliminary, and the adjustment for price changes 
+        in 2021 is based on the consumer price research series, which uses 
+        actual energy prices paid. This aligns more closely with the price 
+        developments experienced by the population than the consumer price index.
+    """,
+    "Private Solar Energy": """
+        **Welcome to the Private Solar Energy indicator**
+        - This indicator is part of the Natural Capital theme.
+        - 2021 and 2022 figures are preliminary.
+    """,
+    "Nature and Forest Areas": """
+        **Welcome to the Nature and Forest Areas indicator**
+        - This indicator is part of the Natural Capital theme.
+    """,
+    "Built-up Area": """
+        **Welcome to the Built-up Area indicator**
+        - This indicator is part of the Natural Capital theme.
+    """,
+    "Fine Particulate Matter Emissions into Air": """
+        **Welcome to the Fine Particulate Matter Emissions into Air indicator**
+        - This indicator is part of the Natural Capital theme.
+    """,
+    "Phosphate Emissions from Agriculture": """
+        **Welcome to the Phosphate Emissions from Agriculture indicator**
+        - This indicator is part of the Natural Capital theme.
+    """,
+    "Green-Blue Space (Excluding Regular Agriculture)": """
+        **Welcome to the Green-Blue Space (Excluding Regular Agriculture) indicator**
+        - This indicator is part of the Natural Capital theme.
+    """,
+    "Nitrogen Emissions from Agriculture": """
+        **Welcome to the Nitrogen Emissions from Agriculture indicator**
+        - This indicator is part of the Natural Capital theme.
+    """,
+    "Weekly Working Hours": """
+        **Welcome to the Weekly Working Hours indicator**
+        - This indicator is part of the Human Capital theme.
+        - 2021 and 2022 figures are preliminary.
+    """,
+    "Highly Educated Population": """
+        **Welcome to the Highly Educated Population indicator**
+        - This indicator is part of the Human Capital theme.
+    """,
+    "Perceived Health": """
+        **Welcome to the Perceived Health indicator**
+        - This indicator is part of the Human Capital theme.
+        - For the years 2012 and 2016, the measured population is 19+.
+    """,
+    "Social Cohesion": """
+        **Welcome to the Social Cohesion indicator**
+        - This indicator is part of the Social Capital theme.
+    """
+}
