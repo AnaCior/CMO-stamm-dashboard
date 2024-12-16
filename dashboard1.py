d785ccc62d1234a67b45cdd52ab74700415235d6
AnaCior <118361884+AnaCior@users.noreply.github.com>
2024-12-16 18:48:00 +0100

    Update dashboard1.py

--- dashboard1.py
+++ dashboard1.py
@@ -39,7 +39,7 @@ st.markdown("""
 
 # Load data
 try:
-    names = pd.read_excel("selected_column.xlsx")
+    names = pd.read_excel("selected_column.xlsx", engine='openpyxl')
 except FileNotFoundError:
     st.error("Error: 'selected_column.xlsx' not found.")
     st.stop()
