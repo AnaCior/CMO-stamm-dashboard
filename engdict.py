1612214c4f4e75d74eb3a587156a615f7fff0644
AnaCior <118361884+AnaCior@users.noreply.github.com>
2025-01-12 19:01:54 +0100

    Update engdict.py

--- engdict.py
+++ engdict.py
@@ -7,224 +7,260 @@ Themes = {
         - Preliminary figures. When a new year is added, the model re-estimates 
         all years in the series. Refer to the Technical Explanation for more 
         details on interpreting model estimates and margins.
+        Measuring unit: Percentage of Dutch people give life a 7 or higher
     """,
-    "Leisure Satisfaction": """
-        **Welcome to the Leisure Satisfaction indicator**
+    "Satisfaction with free time": """
+        **Welcome to the Satisfaction with free time indicator**
         - This indicator is part of the Subjective Well-being theme.
         - Preliminary figures. When a new year is added, the model re-estimates 
         all years in the series. Refer to the Technical Explanation for more 
         details on interpreting model estimates and margins.
+        Measuring unit: Percentage of the population aged 18+ is (very) satisfied
     """,
-    "Median Disposable Income": """
+    "Median disposable income": """
         **Welcome to the Median Disposable Income indicator**
         - This indicator is part of the Material Well-being theme.
         - 2021 figures are preliminary, and the adjustment for price changes in 
         2021 is based on the consumer price research series, which uses actual 
         energy prices paid. This aligns more closely with the price developments 
         experienced by the population than the consumer price index.
+        Measuring unit: Euro per household corrected for inflation, in constant 2021 prices
     """,
     "Gross Domestic Product": """
         **Welcome to the Gross Domestic Product indicator**
         - This indicator is part of the Material Well-being theme.
         - 2022 figures are preliminary.
+        Measuring unit: Euro per inhabitant (2015 prices)
     """,
     "Overweight": """
         **Welcome to the Overweight indicator**
         - This indicator is part of the Health theme.
         - For the year 2016, the measured population is 19+.
+        Measuring unit: Percentage of the population aged 18+ is overweight
     """,
-    "Perceived Health": """
-        **Welcome to the Perceived Health indicator**
+    "Experienced health": """
+        **Welcome to the Experienced Health indicator**
         - This indicator is part of the Health theme.
         - For the years 2012 and 2016, the measured population is 19+.
+        Measuring unit: Percentage of the population aged 18+ consider their own health to be (very) good
+        Measuring unit: Percentage of the population aged 18+ consider their own health to be (very) good
     """,
-    "Life Expectancy of the Population": """
+    "Life expectancy of the population": """
         **Welcome to the Life Expectancy of the Population indicator**
         - This indicator is part of the Health theme.
         - Average life expectancy over the period 2018-2021, not determined 
         separately per year.
+        Measuring unit: The life expentancy in years at birth
     """,
-    "People with One or More Chronic Illnesses or Conditions": """
-        **Welcome to the People with One or More Chronic Illnesses or Conditions indicator**
+    "People with one or more long-term illnesses or conditions": """
+        **Welcome to the People with one or more long-term illnesses or conditions indicator**
         - This indicator is part of the Health theme.
         - For the years 2012 and 2016, the measured population is 19+.
+        Measuring unit: Percentage of population aged 18 years or older
     """,
-    "Net Labor Participation": """
+    "Net labor participation": """
         **Welcome to the Net Labor Participation indicator**
         - This indicator is part of the Work and Leisure theme.
+        Measuring unit: Percentage of the population aged 15 to 74
     """,
-    "Gross Labor Participation": """
+    "Gross labor participation": """
         **Welcome to the Gross Labor Participation indicator**
         - This indicator is part of the Work and Leisure theme.
+        Measuring unit: Percentage of the population aged 15 to 74
     """,
-    "Highly Educated Population": """
+    "Highly educated population": """
         **Welcome to the Highly Educated Population indicator**
         - This indicator is part of the Work and Leisure theme.
+        Measuring unit: Percentage of the population (15 to 74 years) that has completed higher education
     """,
     "Unemployment": """
         **Welcome to the Unemployment indicator**
         - This indicator is part of the Work and Leisure theme.
+        Measuring unit: Percentage of the working population
     """,
-    "Vacancy Rate": """
+    "Vacancy rate": """
         **Welcome to the Vacancy Rate indicator**
         - This indicator is part of the Work and Leisure theme.
         - 2021 and 2022 figures are preliminary.
+        Measuring unit: Number of vacancies per 1,000 jobs
     """,
-    "Distance to Public Transport": """
+    "Distance to public transport": """
         **Welcome to the Distance to Public Transport indicator**
         - This indicator is part of the Work and Leisure theme.
+        Measuring unit: Kilometers
     """,
-    "Satisfaction with Living Environment": """
+    "Satisfaction with living environment": """
         **Welcome to the Satisfaction with Living Environment indicator**
         - This indicator is part of the Housing theme.
+        Measuring unit: Percentage of private households are (very) satisfied
     """,
-    "Satisfaction with Housing": """
+    "Satisfaction with housing": """
         **Welcome to the Satisfaction with Housing indicator**
         - This indicator is part of the Housing theme.
+        Measuring unit: Percentage of private households are (very) satisfied
     """,
-    "Distance to Sports Facility": """
+    "Distance to sports field": """
         **Welcome to the Distance to Sports Facility indicator**
         - This indicator is part of the Housing theme.
+        Measuring unit: Kilometers
     """,
-    "Distance to Primary School": """
+    "Distance to primary school": """
         **Welcome to the Distance to Primary School indicator**
         - This indicator is part of the Housing theme.
+        Measuring unit: Kilometers
     """,
-    "Distance to Café, etc.": """
+    "Distance to café etc.": """
         **Welcome to the Distance to Café, etc. indicator**
         - This indicator is part of the Housing theme.
+        Measuring unit: Kilometers
     """,
-    "Contact with Family, Friends, or Neighbors": """
+    "Contact with family, friends, or neighbors": """
         **Welcome to the Contact with Family, Friends, or Neighbors indicator**
         - This indicator is part of the Society theme.
         - Preliminary figures. When a new year is added, the model re-estimates 
         all years in the series. Refer to the Technical Explanation for more 
         details on interpreting model estimates and margins.
+        Measuring unit: Percentage of population aged 15+ has contact at least once a week on average
     """,
-    "Trust in Institutions": """
+    "Trust in institutions": """
         **Welcome to the Trust in Institutions indicator**
         - This indicator is part of the Society theme.
         - Preliminary figures. When a new year is added, the model re-estimates 
         all years in the series. Refer to the Technical Explanation for more 
         details on interpreting model estimates and margins.
+        Measuring unit: Percentage of the population aged 15+ has sufficient confidence in institutions.
     """,
-    "Trust in Others": """
+    "Trust in others": """
         **Welcome to the Trust in Others indicator**
         - This indicator is part of the Society theme.
         - Preliminary figures. When a new year is added, the model re-estimates 
         all years in the series. Refer to the Technical Explanation for more 
         details on interpreting model estimates and margins.
+        Measuring unit: Percentage of population aged 15+ find most people trustworthy
     """,
-        "Volunteering": """
+        "Volunteer work": """
         **Welcome to the Volunteering indicator**
         - This indicator is part of the Society theme.
         - Preliminary figures. When a new year is added, the model re-estimates 
         all years in the series. Refer to the Technical Explanation for more 
         details on interpreting model estimates and margins.
+        Measuring unit: Percentage of population aged 15 and over doing organised volunteer work in the current year
     """,
-    "Often Feeling Unsafe in the Neighborhood": """
+    "Often feeling unsafe in the neighborhood": """
         **Welcome to the Often Feeling Unsafe in the Neighborhood indicator**
         - This indicator is part of the Safety theme.
+        Measuring unit: Percentage of the population aged 15+ that often feels unsafe.
     """,
-    "Number of Experienced Crimes": """
-        **Welcome to the Number of Experienced Crimes indicator**
+    "Number of crimes encountered": """
+        **Welcome to the Number of Crimes Encountered indicator**
         - This indicator is part of the Safety theme.
+        Measuring unit: Number of crimes experienced per 100 inhabitants
     """,
-    "Registered Crimes": """
-        **Welcome to the Registered Crimes indicator**
+    "Recorded crimes": """
+        **Welcome to the Recorded Crimes indicator**
         - This indicator is part of the Safety theme.
         - 2021 and 2022 figures are preliminary.
+        Measuring unit: Registered crimes per 1000 inhabitants
     """,
-    "Nature Area per Inhabitant": """
+    "Nature area per inhabitant": """
         **Welcome to the Nature Area per Inhabitant indicator**
         - This indicator is part of the Environment theme.
+        Measuring unit: ha per 1000 inhabitants
     """,
-    "Fine Particulate Matter Emissions into Air": """
-        **Welcome to the Fine Particulate Matter Emissions into Air indicator**
+    "Emissions of particulate matter to air": """
+        **Welcome to the Emissions of Particulate Matter to Air indicator**
         - This indicator is part of the Environment theme.
+        Measuring unit: microgram PM 2.5 per m^3
     """,
-    "Distance to Public Green Spaces": """
-        **Welcome to the Distance to Public Green Spaces indicator**
+    "Distance to public green areas": """
+        **Welcome to the Distance to Public Green Areas indicator**
         - This indicator is part of the Environment theme.
+        Measuring unit: Kilometers
     """,
-    "Nature and Forest Areas": """
+    "Nature and forest areas": """
         **Welcome to the Nature and Forest Areas indicator**
         - This indicator is part of the Environment theme.
+        Measuring unit: Percentage of land area
     """,
-    "Greenhouse Gas Emissions per Inhabitant": """
+    "Greenhouse gas emissions per capita": """
         **Welcome to the Greenhouse Gas Emissions per Inhabitant indicator**
         - This indicator is part of the Environment theme.
         - The entire series has been adjusted due to new IPCC guidelines.
+        Measuring unit: Tonnes of CO2 equivalent per inahbitant
     """,
-    "Quality of Swimming Water in Inland Waters": """
+    "Quality of inland bathing water": """
         **Welcome to the Quality of Swimming Water in Inland Waters indicator**
         - This indicator is part of the Environment theme.
+        Measuring unit: Average quality (1=poor to 4=excellent)
     """,
-    "Quality of Swimming Water in Coastal Waters": """
+    "Quality of bathing water coastal waters": """
         **Welcome to the Quality of Swimming Water in Coastal Waters indicator**
         - This indicator is part of the Environment theme.
+        Measuring unit: Average quality (1=poor to 4=excellent)
     """,
-    "Average Household Debt": """
+    "Average debt per household": """
         **Welcome to the Average Household Debt indicator**
         - This indicator is part of the Economic Capital theme.
         - 2022 figures are preliminary, and the adjustment for price changes 
         in 2021 and 2022 is based on the consumer price research series, which 
         uses actual energy prices paid. This aligns more closely with the price 
         developments experienced by the population than the consumer price index.
+        Measuring unit: Euro average debt per household
     """,
-    "Median Household Wealth": """
+    "Median household wealth": """
         **Welcome to the Median Household Wealth indicator**
         - This indicator is part of the Economic Capital theme.
         - 2021 figures are preliminary, and the adjustment for price changes 
         in 2021 is based on the consumer price research series, which uses 
         actual energy prices paid. This aligns more closely with the price 
         developments experienced by the population than the consumer price index.
+        Measuring unit: Euro
     """,
-    "Private Solar Energy": """
+    "Private solar energy": """
         **Welcome to the Private Solar Energy indicator**
         - This indicator is part of the Natural Capital theme.
         - 2021 and 2022 figures are preliminary.
+        Measuring unit: Watts per home
     """,
-    "Nature and Forest Areas": """
+    "Nature and forest areas": """
         **Welcome to the Nature and Forest Areas indicator**
         - This indicator is part of the Natural Capital theme.
+        Measuring unit: Percentage of land area
     """,
-    "Built-up Area": """
+    "Built-up area": """
         **Welcome to the Built-up Area indicator**
         - This indicator is part of the Natural Capital theme.
+        Measuring unit: Percentage of land area
     """,
-    "Fine Particulate Matter Emissions into Air": """
-        **Welcome to the Fine Particulate Matter Emissions into Air indicator**
-        - This indicator is part of the Natural Capital theme.
-    """,
-    "Phosphate Emissions from Agriculture": """
+    "Phosphate excretion agriculture": """
         **Welcome to the Phosphate Emissions from Agriculture indicator**
         - This indicator is part of the Natural Capital theme.
+        Measuring unit: kg per ha of fertilized cultivated land
     """,
-    "Green-Blue Space (Excluding Regular Agriculture)": """
+    "Green-blue space, excluding regular agriculture": """
         **Welcome to the Green-Blue Space (Excluding Regular Agriculture) indicator**
         - This indicator is part of the Natural Capital theme.
+        Measuring unit: 
     """,
-    "Nitrogen Emissions from Agriculture": """
+    "Nitrogen excretion agriculture": """
         **Welcome to the Nitrogen Emissions from Agriculture indicator**
         - This indicator is part of the Natural Capital theme.
+        Measuring unit: Kg per ha of fertilized cultivated land
     """,
-    "Weekly Working Hours": """
+    "Working hours per week": """
         **Welcome to the Weekly Working Hours indicator**
         - This indicator is part of the Human Capital theme.
         - 2021 and 2022 figures are preliminary.
+        Measuring unit: Number of hours of actual hours worked per worker per week
     """,
-    "Highly Educated Population": """
+    "Highly educated population": """
         **Welcome to the Highly Educated Population indicator**
         - This indicator is part of the Human Capital theme.
+        Measuring unit: Percentage of the population (15 to 74 years) that has completed higher education
     """,
-    "Perceived Health": """
-        **Welcome to the Perceived Health indicator**
-        - This indicator is part of the Human Capital theme.
-        - For the years 2012 and 2016, the measured population is 19+.
-    """,
-    "Social Cohesion": """
+    "Social cohesion": """
         **Welcome to the Social Cohesion indicator**
         - This indicator is part of the Social Capital theme.
+        Measuring unit: Scale score (0-10)
     """
 }
