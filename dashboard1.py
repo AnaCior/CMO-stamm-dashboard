c741fb0fe5cf9b688db2c8b1a8beacc5536723d3
AnaCior <118361884+AnaCior@users.noreply.github.com>
2025-01-13 20:17:28 +0100

    Update dashboard1.py

--- dashboard1.py
+++ dashboard1.py
@@ -73,10 +73,7 @@ with col[0]:
     title_base = file_info["title"]
 
     # Load the selected file
-    try:
-        indicator = gpd.read_file(indicator_path)
-    except Exception as e:
-        print(f"Error loading file: {e}")
+    indicator = gpd.read_file(indicator_path)
 
     # Get the column corresponding to the selected year
     selected_column = year_columns[selected_year]
